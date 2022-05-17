ec1afdce03164a71d9d8dc723b6facc8223839b4
Jarpletz <rothpletz5@gmail.com>
2022-05-17 13:12:01 -0400

    posaiuh

---
+++ Relay.py
@@ -0,0 +1,16 @@
+import time
+import RPi.GPIO as GPIO
+import Manager
+
+class relay:
+    def run(OnOff):
+        if OnOff==true:
+                GPIO.output(pin, GPIO.HIGH)
+                time.sleep(1)
+        else:
+                GPIO.output(pin. GPIO.LOW)
+                time.sleep(1)
+    def __init_(self, OnOff):
+        self.pin=37
+        self.GPIO.setmode(GPIO.BOARD)
+        self.GPIO.setup(pin, GPIO.OUT)
