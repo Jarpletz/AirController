80e598d659c2b5f866e9e5d96534291216724e8a
Jarpletz <rothpletz5@gmail.com>
2022-05-18 15:15:36 -0400

    fixed some relay bugs

--- Relay.py
+++ Relay.py
@@ -2,14 +2,15 @@ import time
 import RPi.GPIO as GPIO
 
 class relay:
-     def __init_(self):
+     def __init__(self):
         self.pin=37
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
 
      def run(self,OnOff):
         if OnOff==False:
-                GPIO.output(self.pin, GPIO.HIGH)
+                #GPIO.output(self.pin, GPIO.HIGH)
+                print(self.pin)
                 #time.sleep(1)
         else:
                 GPIO.output(self.pin, GPIO.LOW)
